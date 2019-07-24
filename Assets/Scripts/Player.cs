24bf5564c8c915933fa402817503e52ff485dc1f
anlideer <643525796@qq.com>
2019-07-24 13:53:54 +0800

    Version0.0

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public static Player Instance;
+
+    public GameObject[] Bones;
+    public SkinnedMeshRenderer SMRenderer;
+    public Mesh playerBakedMesh;
+
+    Vector3 Center;
+
+
+    void Awake ()
+    {
+        Instance = this;
+	}
+	
+	void FixedUpdate ()
+    {
+		for(int i = 0; i < Bones.Length; i++)
+        {
+            Center += Bones[i].transform.position;
+        }
+
+        Center = Center / Bones.Length;
+
+        transform.position = Center;
+
+        Center = Vector3.zero;
+
+        PlayerBakeMeshToCollider();
+	}
+
+    void PlayerBakeMeshToCollider()
+    {
+        SMRenderer.BakeMesh(playerBakedMesh);
+        gameObject.GetComponent<MeshCollider>().sharedMesh = playerBakedMesh;
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Jelly
- Simulate Jelly effects in Unity3D.
+ Simulate Jelly effects in Unity3D. 
+ Refer to https://www.youtube.com/watch?v=UxLJ6XewTVs and https://www.youtube.com/watch?v=tTdKEJpX2HI.
